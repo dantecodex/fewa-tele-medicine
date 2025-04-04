5b43b0a0e9829c72180706f50373ece2b9619e11
saurabhjain <saurabh.jdev777@gmail.com>
2025-04-04 18:32:17 +0530

    work in progress for redis-bullmq

--- server/src/config/redis.config.js
+++ server/src/config/redis.config.js
@@ -1,17 +1,9 @@
 import { Redis } from "ioredis"
 
 const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    // password: process.env.REDIS_PASSWORD,
+    host: "localhost",
+    port: 6379,
+    password: "mysecretpassword"
 })
-
-redis.on('connect', () => {
-    console.log('Connected to Redis');
-});
-
-redis.on('error', (err) => {
-    console.error('Redis error', err);
-});
 
 export default redis

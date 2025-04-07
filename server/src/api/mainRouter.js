a7b8bc3801bf3753b163d8620195ac7762b1088f
saurabhjain <saurabh.jdev777@gmail.com>
2025-04-07 18:44:28 +0530

    added user update api

--- server/src/api/mainRouter.js
+++ server/src/api/mainRouter.js
@@ -1,25 +1,16 @@
 import express from "express"
 
-import authController from "../controller/auth.controller.js"
 import userController from "../controller/user.controller.js"
 
 import checkAuth from "../middleware/checkAuth.js"
-import rateLimiter from "../utils/rateLimiter.js"
+import authRouter from "./auth.router.js"
 
 const apiRouter = express.Router()
 
-apiRouter.route("/auth/signup").post(rateLimiter(10, "15min"), authController.signup)
-apiRouter.route("/auth/login").post(rateLimiter(5, "10min"), authController.login)
-apiRouter.route("/auth/resend-verify-email").post(rateLimiter(3, "10min"), authController.resendVerifyEmailOTP)
-apiRouter.route("/auth/verify-email").post(rateLimiter(5, "10min"), authController.verifyEmail)
-
-apiRouter.route("/auth/forgot-password")
-  .post(rateLimiter(5, "10min"), authController.sendForgotPasswordOTP)
-  .get(rateLimiter(5, "10min"), authController.verifyForgotPasswordOTP)
-  .patch(rateLimiter(5, "10min"), authController.resetForgotPassword)
+apiRouter.use(authRouter) // Authentication related routes
 
 apiRouter.use(checkAuth)
 
-apiRouter.route("/user/profile").get(userController.profile).put(userController.updateProfile)
+apiRouter.route("/user/profile").put(userController.updateProfile)
 
 export default apiRouter
--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -3,18 +3,21 @@ import apiResponseHandler from "../utils/apiResponseHandler.js"
 import asyncErrorHandler from "../utils/asyncErrorHandler.js"
 import userValidation from "../validation/user.validation.js"
 
-const profile = asyncErrorHandler(async (req, res) => {
-  const profileData = await userService.profileData(req.user.id)
-
-  res.status(200).json(apiResponseHandler("User profile details have been fetched", profileData))
-})
 const updateProfile = asyncErrorHandler(async (req, res) => {
-  const validatedData = userValidation.updateProfile.validate(req.body)
+  const { role, ...profileData } = req.body
+
+  const validatorMap = {
+    DOCTOR: userValidation.doctorProfile,
+    PATIENT: userValidation.patientProfile,
+  }
+
+  const validator = validatorMap[role] || userValidation.patientProfile
+  const validatedData = validator.validate(profileData)
+
   const updatedUser = await userService.updateProfile(validatedData, req.user.id)
 
   res.status(200).json(apiResponseHandler("User has been updated successfully", updatedUser))
 })
 export default {
-  profile,
   updateProfile,
 }

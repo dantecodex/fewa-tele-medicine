50d715ed8d1df05c6a993cd0251c349190f27085
saurabhjain <saurabh.jdev777@gmail.com>
2025-04-14 12:54:53 +0530

    added zoom and redis functionality

--- server/src/config/redis.config.js
+++ server/src/config/redis.config.js
@@ -1,9 +1,17 @@
 import { Redis } from "ioredis"
 
 const redis = new Redis({
-    host: "localhost",
-    port: 6379,
-    password: "mysecretpassword"
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    // password: process.env.REDIS_PASSWORD,
 })
+
+redis.on('connect', () => {
+    console.log('Connected to Redis');
+});
+
+redis.on('error', (err) => {
+    console.error('Redis error', err);
+});
 
 export default redis

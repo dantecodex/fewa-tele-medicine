26923e7510938db42aa17932916d46fe98624705
saurabhjain <saurabh.jdev777@gmail.com>
2025-04-14 12:54:53 +0530

    added zoom and redis functionality

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -4,7 +4,7 @@ const logger = winston.createLogger({
   level: process.env.LOG_LEVEL || "info",
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
-    new winston.transports.Console(),
+    // new winston.transports.Console(),
     new winston.transports.File({
       filename: "logs/combined.log",
       maxsize: 5 * 1024 * 1024, // 5MB

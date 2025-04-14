544a8630e014ba9a71f09bd1b25cdd723012b85c
saurabhjain <saurabh.jdev777@gmail.com>
2025-04-14 17:03:35 +0530

    Added role-based access and increase jwt token expiry time

--- server/src/api/mainRouter.js
+++ server/src/api/mainRouter.js
@@ -6,15 +6,16 @@ import checkAuth from "../middleware/checkAuth.js"
 import authRouter from "./auth.router.js"
 import Upload from "../middleware/multer.js"
 import zoomController from "../controller/zoom.controller.js"
+import authorizeRole from "../middleware/authorization.js"
 
 const apiRouter = express.Router()
 
 apiRouter.use(authRouter) // Authentication related routes
 
-apiRouter.use(checkAuth)
+apiRouter.use(checkAuth) // Middleware to check if the user exist or not
 
 apiRouter.route("/user/profile").put(Upload().single('avatar'), userController.updateProfile)
 
-apiRouter.route("/zoom/create-meeting").post(zoomController.createMeeting)
+apiRouter.route("/zoom/create-meeting").post(authorizeRole('ADMIN'), zoomController.createMeeting)
 
 export default apiRouter

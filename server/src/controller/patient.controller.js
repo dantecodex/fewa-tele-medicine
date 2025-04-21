c00f8c39291d4c5bf74a7d972a06b03ba9757ed2
saurabhjain <saurabh.jdev777@gmail.com>
2025-04-21 17:20:04 +0530

    Enhance patientList response with structured API response handling

--- server/src/controller/patient.controller.js
+++ server/src/controller/patient.controller.js
@@ -1,8 +1,10 @@
 import patientService from "../service/patient.service.js";
+import apiResponseHandler from "../utils/apiResponseHandler.js";
 import asyncErrorHandler from "../utils/asyncErrorHandler.js";
 
 const patientList = asyncErrorHandler(async (req, res) => {
     const list = await patientService.patientList()
+    res.status(200).json(apiResponseHandler("Patient list has been fetched", list))
 })
 
 export default {
